9031070b92269fafc6e88a8c1a9ea530da17bcd0
unknown <kganev26@gmail.com>
2025-05-21 21:00:29 +0300

    FormHTMLandFixedThings

--- Kaloyan_Ganev/search.js
+++ Kaloyan_Ganev/search.js
@@ -17,12 +17,19 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!isNaN(number) && number >= -8 && number <= 5) {
             switch(number)
             {
-                case -1: window.location.href = '-1vek.html';break;
-                case 1: window.location.href = '1vek.html';break;
-                case 2: window.location.href = '2vek.html';break;
-                case 3: window.location.href = '3vek.html';break;
-                case 4: window.location.href = '4vek.html';break;
-                case 5: window.location.href = '5vek.html';break;
+                case -8: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/8vprhr.html';break;
+                case -7: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/7vprhr.html';break;
+                case -6: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/6vprhr.html';break;
+                case -5: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/5vprhr.html';break;
+                case -4: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/4vprhr.html';break;
+                case -3: window.location.href = 'C:/8A-24-25-Team4/Daniel_Filipov/3vprhr.html';break; 
+                case -2: window.location.href = 'C:/8A-24-25-Team4/Daniel_Rashkov/-2vek.html';break;
+                case -1: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/-1vek.html';break;
+                case 1: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/1vek.html';break;
+                case 2: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/2vek.html';break;
+                case 3: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/3vek.html';break;
+                case 4: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/4vek.html';break;
+                case 5: window.location.href = 'C:/8A-24-25-Team4/Kaloyan_Ganev/5vek.html';break;            
             }
             } else {
                 alert('Invalid number. Please enter a number between -8 and 5.');
